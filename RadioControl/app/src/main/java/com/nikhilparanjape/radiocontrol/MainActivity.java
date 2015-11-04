559b318830aaeee178e50a68ae432c1b72cb4797
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2015-11-03 19:05:22 -0500

    2nd commit
    Fixed a gradle bug, restarted program
    Created icon

--- RadioControl/app/src/main/java/com/nikhilparanjape/radiocontrol/MainActivity.java
+++ RadioControl/app/src/main/java/com/nikhilparanjape/radiocontrol/MainActivity.java
@@ -1,12 +1,13 @@
 package com.nikhilparanjape.radiocontrol;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
