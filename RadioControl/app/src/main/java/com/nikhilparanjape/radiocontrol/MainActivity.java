21bb254cf9c0c64ddb5f34ff0bac4ec6d52ae0cc
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2015-11-07 22:27:37 -0500

    v1.0.1 - Bluetooth
    Check for bluetooth and reenable as necessary
    
    Added comments :)

--- RadioControl/app/src/main/java/com/nikhilparanjape/radiocontrol/MainActivity.java
+++ RadioControl/app/src/main/java/com/nikhilparanjape/radiocontrol/MainActivity.java
@@ -2,6 +2,7 @@ package com.nikhilparanjape.radiocontrol;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
@@ -42,8 +43,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialize the process
         Process p;
+        //Root commands for airplane mode
         String[] airplaneCmd = {"su", "settings put global airplane_mode_on 1", "am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true","svc wifi enable"};
+        //Root commands if there is an active bluetooth connection
+        String[] bluetoothCmd = {"su", "settings put global airplane_mode_on 1", "am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true","svc wifi enable","service call bluetooth_manager 6"};
+        //Root commands if there is a wifi connection. NOT USED
         String[] wifiCmd = {"su", "settings put global airplane_mode_on 1", "am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true"};
         Context context = this;
         //Initialize network settings
@@ -52,44 +58,69 @@ public class MainActivity extends Activity {
 
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
+        // Check if the device is connected to the internet
         boolean isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
 
         //booleans for radios
+
+        //Check for bluetooth
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        //Check for airplane mode
         boolean isEnabled = Settings.System.getInt(this.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) == 1;
-        String test = Boolean.toString(isEnabled);
+        String test = Boolean.toString(isEnabled); // A test string, not needed
 
         if (isConnected && isEnabled == false) {
-            boolean isWiFi = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+            boolean isWiFi = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI; //Boolean to check for an active WiFi connection
+            //Check for wifi
             if(isWiFi) {
-                try {
-                    p = Runtime.getRuntime().exec("su");
-                    DataOutputStream os = new DataOutputStream(p.getOutputStream());
-                    for (String tmpCmd : airplaneCmd) {
-                        os.writeBytes(tmpCmd + "\n");
+                //If the bluetooth connection is on
+                if(bluetoothAdapter.isEnabled()) {
+                    try {
+                        p = Runtime.getRuntime().exec("su");
+                        DataOutputStream os = new DataOutputStream(p.getOutputStream());
+                        for (String tmpCmd : bluetoothCmd) {
+                            os.writeBytes(tmpCmd + "\n");
+                        }
+                        os.writeBytes("exit\n");
+                        os.flush();
+                    } catch (IOException e) {
+
                     }
-                    os.writeBytes("exit\n");
-                    os.flush();
-                } catch (IOException e) {
-
+                    Log.d("Wifi", "wifi is on" + test);
                 }
-                Log.d("Wifi", "wifi is on" + test);
+                //If bluetooth is off, run the standard root request
+                else if(bluetoothAdapter == null){
+                    try {
+                        p = Runtime.getRuntime().exec("su"); //Request SU
+                        DataOutputStream os = new DataOutputStream(p.getOutputStream()); //Used for terminal
+                        for (String tmpCmd : airplaneCmd) {
+                            os.writeBytes(tmpCmd + "\n"); //Sends commands to the terminal
+                        }
+                        os.writeBytes("exit\n"); //Quits the terminal session
+                        os.flush(); //Ends datastream
+                    } catch (IOException e) {
+                        Log.d("Root", "There was an error with root");
+                    }
+                    Log.d("Wifi", "wifi is on" + test);
+                }
             }
-
+        //Check if airplane mode is on, and wifi is off
+        //This means user is in a no WiFi and no cell range
         } else if(isConnected == false && isEnabled == true){
-
+            Log.d("Radio","I didn't have to do anything, yay");
         }
 
         else {
             Log.d("Else","something is different");
         }
 
-
+        //Creates navigation drawer header
         AccountHeader headerResult = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withHeaderBackground(R.mipmap.header)
                 .addProfiles(
-                        new ProfileDrawerItem().withName(getDeviceName()).withEmail("v1.0-ALPHA")
+                        new ProfileDrawerItem().withName(getDeviceName()).withEmail("v1.0.1-ALPHA")
                 )
                 .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
                     @Override
@@ -98,11 +129,13 @@ public class MainActivity extends Activity {
                     }
                 })
                 .build();
-
+        //Creates navigation drawer items
         PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Home").withIcon(GoogleMaterial.Icon.gmd_wifi);
         SecondaryDrawerItem item4 = new SecondaryDrawerItem().withName("About").withIcon(GoogleMaterial.Icon.gmd_help);
         SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("Settings").withIcon(GoogleMaterial.Icon.gmd_settings);
         SecondaryDrawerItem item3 = new SecondaryDrawerItem().withName("About").withIcon(GoogleMaterial.Icon.gmd_info);
+
+        //Create navigation drawer
         Drawer result = new DrawerBuilder()
                 .withAccountHeader(headerResult)
                 .withActivity(this)
@@ -124,6 +157,8 @@ public class MainActivity extends Activity {
 
     }
 
+
+    //Grab device make and model for drawer
     public String getDeviceName() {
         String manufacturer = Build.MANUFACTURER;
         String model = Build.MODEL;
@@ -133,17 +168,7 @@ public class MainActivity extends Activity {
             return capitalize(manufacturer) + " " + model;
         }
     }
-
-    public void toggleWiFi(boolean status) {
-        WifiManager wifiManager = (WifiManager) this
-                .getSystemService(Context.WIFI_SERVICE);
-        if (status == true && !wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(true);
-        } else if (status == false && wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(false);
-        }
-    }
-
+    //Capitalizes names for devices. Used by getDeviceName()
     private String capitalize(String s) {
         if (s == null || s.length() == 0) {
             return "";
