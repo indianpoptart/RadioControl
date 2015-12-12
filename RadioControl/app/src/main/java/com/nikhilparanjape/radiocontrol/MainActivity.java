eb6351685e303d773ba6be4a422f7886c0dcf708
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2015-12-12 05:52:35 -0500

    v1.3 - Release
    Release commit

--- RadioControl/app/src/main/java/com/nikhilparanjape/radiocontrol/MainActivity.java
+++ RadioControl/app/src/main/java/com/nikhilparanjape/radiocontrol/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                 .withActivity(this)
                 .withHeaderBackground(R.mipmap.header)
                 .addProfiles(
-                        new ProfileDrawerItem().withName(getDeviceName()).withEmail("v1.3 - Alpha")
+                        new ProfileDrawerItem().withName(getDeviceName()).withEmail("v1.3")
                 )
                 .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
                     @Override
